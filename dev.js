bcbe0ecfbf4e4ea288ecc1384a2eb8938580cb0d
Ola Valø Pettersen <ola@24nettbutikk.no>
2025-07-30 16:23:49 +0200

    setup server

--- dev.js
+++ dev.js
@@ -6,13 +6,7 @@ import { exec } from 'child_process'
 const app = express()
 const PORT = 3001
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'dist')))
-
 app.get('/calendars/ola/calendar.ics', async (req, res) => {
     const ICS_URL = 'https://calendar.google.com/calendar/ical/c_b7d573fbbb7b74fb3515bea647d8d483e1994b07d5fb277105ca51bbca171833%40group.calendar.google.com/private-9966217655c940ae74830a22fe0e6686/basic.ics'
     try {
@@ -36,9 +30,6 @@ app.post('/shutdown', (req, res) => {
     })
 })
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
 
 app.listen(PORT, () => {
     console.log(`ICS proxy server running at http://localhost:${PORT}`)
