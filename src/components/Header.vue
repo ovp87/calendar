e70840649535964b20065255cc225ec2c3c2c6c5
Ola Valø Pettersen <ola@24nettbutikk.no>
2025-07-31 04:12:19 +0200

    make buttons buttons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,9 +18,18 @@
 
 <template>
   <div class="relative w-full mb-8">
-    <PhotoIcon @click="emit('setMode', 'photo')" :class="[mode === 'photo' ? 'text-blue-600' : 'text-gray-500']" class="absolute top-0 right-36 h-20 w-20 cursor-pointer px-4" />
-    <NumberedListIcon @click="emit('setMode', 'weekly-list')" :class="[mode === 'weekly-list' ? 'text-blue-600' : 'text-gray-500']" class="absolute top-0 right-18 h-20 w-20 cursor-pointer px-4" />
-    <CalendarDaysIcon @click="emit('setMode', 'weekly-calendar')" :class="[mode === 'weekly-calendar' ? 'text-blue-600' : 'text-gray-500']" class="absolute top-0 right-2 h-20 w-20 cursor-pointer px-4" />
+    <button @click="emit('setMode', 'photo')" type="button" class="absolute top-0 left-2 h-20 w-20 cursor-pointer px-4">
+      <span class="sr-only">Photo slideshow</span>
+      <PhotoIcon :class="[props.mode === 'photo' ? 'text-blue-600' : 'text-gray-500']" class="h-20 w-20" />
+    </button>
+    <button @click="emit('setMode', 'weekly-list')" type="button" class="absolute top-0 left-18 h-20 w-20 cursor-pointer px-4">
+      <span class="sr-only">Weekly list view</span>
+      <NumberedListIcon :class="[props.mode === 'weekly-list' ? 'text-blue-600' : 'text-gray-500']" class="h-20 w-20" />
+    </button>
+    <button @click="emit('setMode', 'weekly-calendar')" type="button" class="absolute top-0 right-18 h-20 w-20 cursor-pointer px-4">
+      <span class="sr-only">Weekly calendar view</span>
+      <CalendarDaysIcon :class="[props.mode === 'weekly-calendar' ? 'text-blue-600' : 'text-gray-500']" class="h-20 w-20" />
+    </button>
     <div v-if="showWeekNumber" class="flex items-center justify-center gap-2 mt-4">
       <button @click="prevWeek" type="button" class="cursor-pointer flex h-9 w-12 items-center justify-center rounded-l-md border border-gray-300 pr-1 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:pl-0 md:hover:bg-gray-50">
         <span class="sr-only">Previous week</span>
